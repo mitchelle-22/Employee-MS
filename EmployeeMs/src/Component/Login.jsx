9ea5f9c43f53ae3e260f2d67b4e1afe33bce3131
Mich <mitchellefetty@gmail.com>
2023-10-29 12:09:18 +0200

    landing page in progress

--- EmployeeMs/src/Component/Login.jsx
+++ EmployeeMs/src/Component/Login.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './style.css'
+import axios from 'axios'
 
 function Login() {
+
+    const [values,setValues] = useState({
+        email:'',
+        password:''
+    })
+
+    const handleSubmit =(event) =>{
+        event.preventDefault()
+        axios.post('http://localhoost:3000/auth/adminlogin')
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+
+    }
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
         <div className='p-3 rounded w-25 border loginForm'>
           <h2>Login Page</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='mb-3'>
                 <label htmlFor="email"><strong>Email:</strong></label>
                 <input type="email" name='email' autoComplete='off'placeholder='Enter Email'
-                className='form-control rounded-0'/>
+               onChange={(e)=> setValues({...values,email:e.target.value})} className='form-control rounded-0'/>
             </div>
             <div className='mb-3'>
                 <label htmlFor="password"><strong>Password:</strong></label>
                 <input type="password" name='password' placeholder='Enter Password'
-                className='form-control rounded-0'/>
+                onChange={(e)=> setValues({...values,password:e.target.value})} className='form-control rounded-0'/>
             </div>
-            <button className='btn btn-success w-100 rounded-0'>Submit</button>
-            <div className='mb-3'>
-                <input type="checkbox" name="tick" id="tick" />
-                <label htmlFor="password"><strong>Password:</strong></label>
+            <button className='btn btn-success w-100 rounded-0 mb-2'>Submit</button>
+            <div className='mb-1'>
+                <input type="checkbox" name="tick" id="tick" className="me-2"/>
+                <label htmlFor="password">You are Agree with terms & conditions</label>
             </div>
             </form>  
         </div>
