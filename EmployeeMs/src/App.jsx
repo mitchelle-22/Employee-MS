4cfba0be3e336123a702f1a4ccfcfe5f4be7c1ec
Mich <mitchellefetty@gmail.com>
2023-11-02 12:33:11 +0200

    dashboard nav bar

--- EmployeeMs/src/App.jsx
+++ EmployeeMs/src/App.jsx
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './Component/Login'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Dashboard from './Component/Dashboard'
+import Home from './Component/Home'
+import Employee from './Component/Employee'
+import Category from './Component/Category'
+import Profile from './Component/Profile'
 
 function App() {
 
@@ -12,7 +16,15 @@ function App() {
   <BrowserRouter>
   <Routes>
     <Route path = '/adminlogin' element={<Login/>}></Route>
-    <Route path = '/dashboard' element={<Dashboard/>}></Route>
+    <Route path = '/dashboard' element={<Dashboard/>}>
+    <Route path = '' element={<Home/>}></Route>
+      <Route path = '/dashboard/employee' element={<Employee/>}></Route>
+      <Route path = '/dashboard/category' element={<Category/>}></Route>
+      <Route path = '/dashboard/profile' element={<Profile/>}></Route>
+
+    </Route>
+      
+      
   </Routes>
   </BrowserRouter>
   )
